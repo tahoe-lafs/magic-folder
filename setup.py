cdc5ab8d2420ef825ddd918954f395fb2605762e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-02-05 09:25:02 -0500

    Get the test plugins installed as well

--- setup.py
+++ setup.py
@@ -266,7 +266,7 @@ setup(name="magic_folder", # also set in __init__.py
                 "test": PleaseUseTox,
                 },
       package_dir = {'':'src'},
-      packages=find_packages('src') + ["twisted/plugins"],
+      packages=find_packages('src') + ["twisted.plugins", "magic_folder.test.plugins"],
       classifiers=trove_classifiers,
       python_requires="~=2.7",
       install_requires=install_requires,
