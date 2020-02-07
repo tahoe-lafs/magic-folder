c0b87d461639953d39a761ab2b7517de38cf6c23
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-02-07 15:31:55 -0500

    This shouldn't be in version control.

--- src/magic_folder/_version.py
+++
@@ -1,9 +0,0 @@
-
-# This _version.py is generated from git metadata by the magic_folder setup.py.
-
-__pkgname__ = "magic_folder"
-real_version = "0.0.0-76-g3d1858f-dirty"
-full_version = "3d1858f238ec5524678fa6ed9b93e7f49fe1ee91-dirty"
-branch = "12.towncrier-config"
-verstr = "0.0.0.post76.dev0"
-__version__ = verstr
