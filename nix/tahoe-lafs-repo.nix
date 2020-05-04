730b99541769c4c733016f284eb978f25fbc72b8
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-05-04 22:44:18 +0530

    nix: use the sha256 sum from nix-prefetch-url --unpack --type sha256 <github-url>

--- nix/tahoe-lafs-repo.nix
+++ nix/tahoe-lafs-repo.nix
@@ -5,5 +5,5 @@ in
     owner = "tahoe-lafs";
     repo = "tahoe-lafs";
     rev = "tahoe-lafs-1.14.0";
-    sha256 = "02gz83avmd4n0f22ss9hg1xazgikik13z4k7v8ri1ag1vzy0h6bh";
+    sha256 = "1ahdiapg57g6icv7p2wbzgkwl9lzdlgrsvbm5485414m7z2d6las";
   }
