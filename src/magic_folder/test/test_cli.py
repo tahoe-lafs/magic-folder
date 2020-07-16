39c8a4443d245b82d86f6bedee329f75d9dda631
meejah <meejah@meejah.ca>
2020-07-15 18:04:18 -0600

    fix tests

--- src/magic_folder/test/test_cli.py
+++ src/magic_folder/test/test_cli.py
@@ -18,15 +18,27 @@ from zope.interface import (
 
 import attr
 
+from testtools import (
+    ExpectedException,
+)
+from testtools.matchers import (
+    Equals,
+)
 from .common import (
     AsyncTestCase,
     SyncTestCase,
+)
+from .fixtures import (
+    NodeDirectory,
 )
 from magic_folder.util.observer import (
     ListenObserver,
 )
 from magic_folder.initialize import (
     magic_folder_initialize,
+)
+from magic_folder.migrate import (
+    magic_folder_migrate,
 )
 
 
@@ -92,12 +104,24 @@ class TestMigrate(SyncTestCase):
     """
 
     def setUp(self):
-        super(TestInitialize, self).setUp()
+        super(TestMigrate, self).setUp()
         self.temp = FilePath(self.mktemp())
-
-    def tearDown(self):
-        super(TestInitialize, self).tearDown()
-        rmtree(self.temp.path, ignore_errors=True)
+        from twisted.internet import reactor
+        self.magic_path = self.temp.child("magic")
+        self.magic_path.makedirs()
+        self.node_dir = self.useFixture(NodeDirectory(self.temp.child("node")))
+        self.node_dir.create_magic_folder(
+            u"test-folder",
+            u"URI:DIR2-RO:ou5wvazwlyzmqw7yof5ifmgmau:xqzt6uoulu4f3m627jtadpofnizjt3yoewzeitx47vw6memofeiq",
+            u"URI:DIR2:bgksdpr3lr2gvlvhydxjo2izea:dfdkjc44gg23n3fxcxd6ywsqvuuqzo4nrtqncrjzqmh4pamag2ia",
+            self.magic_path,
+            60,
+        )
 
     def test_good(self):
-        magic_folder_initialize(self.temp.path, u"tcp:1234", u"http://example.com")
+        magic_folder_migrate(
+            self.temp.child("new_magic"),
+            u"tcp:1234",
+            self.node_dir.path,
+            u"alice",
+        )
