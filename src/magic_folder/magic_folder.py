f48a02bc133e6a679caa19adb87e88d47645b627
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-07-17 11:38:48 +0530

    code to debug the unicode error

--- src/magic_folder/magic_folder.py
+++ src/magic_folder/magic_folder.py
@@ -2302,7 +2302,12 @@ class LocalSnapshotCreator(service.Service):
             return magicpath.path2magic(p.asTextMode().path)
 
         with path.open('rb') as input_stream:
-            SNAPSHOT_CREATOR_PROCESS_ITEM.log(relpath=path.asTextMode().path)
+            relpath_u = path.asTextMode().path
+            if not isinstance(relpath_u, unicode):
+                raise ValueError(
+                    "'relpath_u' must be a unicode string"
+                )
+            SNAPSHOT_CREATOR_PROCESS_ITEM.log(relpath=relpath_u)
             snapshot = yield create_snapshot(
                 name=mangle_path(path),
                 author=self.author,
