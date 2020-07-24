8930e24f818be2a5deb3f57ae637791b6a083c89
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-07-24 20:06:42 +0530

    some tests for LocalSnapshotCreator

--- src/magic_folder/test/test_local_snapshot.py
+++ src/magic_folder/test/test_local_snapshot.py
@@ -21,6 +21,8 @@ from testtools.matchers import (
     Not,
     Equals,
     Always,
+    HasLength,
+    MatchesStructure,
 )
 from testtools.twistedsupport import (
     succeeded,
@@ -35,9 +37,13 @@ from eliot import (
 
 from magic_folder.magic_folder import (
     LocalSnapshotService,
+    LocalSnapshotCreator,
 )
 from magic_folder.snapshot import (
     create_local_author,
+)
+from magic_folder.magicpath import (
+    path2magic,
 )
 from .. import magicfolderdb
 
@@ -212,4 +218,99 @@ class LocalSnapshotServiceTests(SyncTestCase):
 
         self.assertThat(self._snapshot_creator.processed.get(foo),
                         Equals(1)
+        )
+
+class LocalSnapshotCreatorTests(SyncTestCase):
+    """
+    Tests for LocalSnapshotCreator that is responsible for
+    creating the local snapshots and storing them in the database.
+    """
+    def setUp(self):
+        # setup db
+        # create author
+        # setup stashdir
+        # setup magicpath dir (the base directory for a particular magic folder)
+        # instantiate LocalSnapshotCreator
+        super(LocalSnapshotCreatorTests, self).setUp()
+        self.db = magicfolderdb.get_magicfolderdb(":memory:", create_version=(magicfolderdb.SCHEMA_v1, 1))
+        self.author = create_local_author("alice")
+
+        self.stash_dir = self.mktemp()
+        os.mkdir(self.stash_dir)
+
+        magic_path_dirname = self.mktemp()
+        os.mkdir(magic_path_dirname)
+        self.magic_path = FilePath(magic_path_dirname)
+
+        self.snapshot_creator = LocalSnapshotCreator(
+            db=self.db,
+            author=self.author,
+            stash_dir=FilePath(self.stash_dir),
+        )
+
+    def setup_example(self):
+        """
+        Hypothesis-invoked hook to create per-example state.
+        Reset the database before running each test.
+        """
+        # XXX: Not sure if this function should exist at all and even if
+        # so, it shouldn't exist in magicfolderdb module perhaps?
+        self.db._clear_snapshot_table()
+
+    @given(lists(path_segments().map(lambda p: p.encode("utf-8")), unique=True),
+           lists(binary(), unique=True))
+    def test_create_snapshots(self, filenames, contents):
+        """
+        Create a list of filenames and random content as input and for each
+        of the (filename, content) mapping, create and store the snapshot in
+        the database.
+        """
+        files = []
+        for (filename, content) in zip(filenames, contents):
+            file = self.magic_path.child(filename)
+            with file.open("wb") as f:
+                f.write(content)
+            files.append(file)
+
+        for file in files:
+            self.snapshot_creator.process_item(file)
+
+        self.assertThat(self.db.get_all_localsnapshot_paths(), HasLength(len(files)))
+        for (file, content) in zip(files, contents):
+            mangled_filename = path2magic(file.asTextMode(encoding="utf-8").path)
+            stored_snapshot = self.db.get_local_snapshot(mangled_filename, self.author)
+            stored_content = stored_snapshot._get_synchronous_content()
+            self.assertThat(stored_content, Equals(content))
+            self.assertThat(stored_snapshot.parents_local, HasLength(0))
+
+    @given(content1=binary(min_size=1),
+           content2=binary(min_size=1),
+           filename=path_segments().map(lambda p: p.encode("utf-8")),
+    )
+    def test_create_snapshot_twice(self, filename, content1, content2):
+        """
+        If a snapshot already exists for a file, adding a new snapshot to it
+        should refer to the existing snapshot as a parent.
+        """
+        foo = self.magic_path.child(filename)
+        with foo.open("wb") as f:
+            f.write(content1)
+
+        self.snapshot_creator.process_item(foo)
+
+        foo_magicname = path2magic(foo.asTextMode('utf-8').path)
+        stored_snapshot1 = self.db.get_local_snapshot(foo_magicname, self.author)
+
+        # now modify the file with some new content.
+        with foo.open("wb") as f:
+            f.write(content2)
+
+        self.snapshot_creator.process_item(foo)
+        stored_snapshot2 = self.db.get_local_snapshot(foo_magicname, self.author)
+
+        self.assertThat(
+            stored_snapshot2.parents_local[0],
+            MatchesStructure(
+                content_path=Equals(stored_snapshot1.content_path)
+            )
         )
