d758b0732086559d31473bfd94cdfd35b5227e43
meejah <meejah@meejah.ca>
2020-07-29 15:02:57 -0600

    re-write to use tahoe_client

--- src/magic_folder/invite.py
+++ src/magic_folder/invite.py
@@ -21,8 +21,10 @@ from twisted.web.http import (
 )
 
 from .common import (
-    tahoe_mkdir,
     INVITE_SEPARATOR,
+)
+from .tahoe_client import (
+    create_tahoe_client,
 )
 
 
@@ -42,6 +44,8 @@ def magic_folder_invite(config, folder_name, invitee_name, treq):
 
     :return Deferred[unicode]: A secret invitation code.
     """
+    # XXX probably want to pass this in, instead of "treq"?
+    tahoe_client = create_tahoe_client(config.tahoe_client_url, treq)
 
     # get configuration for this magic-folder (or error if it doesn't
     # exist)
@@ -52,8 +56,13 @@ def magic_folder_invite(config, folder_name, invitee_name, treq):
             u"No magic-folder named '{}'".format(folder_name)
         )
 
+    if not folder_config.is_admin():
+        raise usage.UsageError(
+            u"This device is not the administrator for '{}'".format(folder_name)
+        )
+
     # create an unlinked directory and get the dmd write-cap
-    dmd_write_cap = yield tahoe_mkdir(config.tahoe_client_url, treq)
+    dmd_write_cap = yield tahoe_client.create_mutable_directory()
 
     # derive a dmd read-only cap from it.
     dmd_readonly_cap = uri.from_string(dmd_write_cap.encode("utf8")).get_readonly().to_string()
@@ -61,39 +70,25 @@ def magic_folder_invite(config, folder_name, invitee_name, treq):
         raise Exception("failed to diminish dmd write cap")
 
     # Now, we need to create a link to the nickname from inside the
-    # collective to this read-cap. For that we will need to know
-    # the write-cap of the collective (which is stored by the private/aliases
-    # file in the node_directory) so that a link can be created inside it
-    # to the .
-    # To do that, we use tahoe ln dmd_read_cap <collective-write-cap>/<alias>
+    # collective to this read-cap. For that we will need to know the
+    # write-cap of the collective (which will only be true if we're
+    # the administrator device) so that a link can be created inside
+    # it.
 
+    # we already checked above that this is a write-cap using .is_admin()
     magic_write_cap = folder_config.collective_dircap
     magic_readonly_cap = uri.from_string(magic_write_cap.encode("utf8")).get_readonly().to_string()
 
-    # similar to:
-    #
-    #   tahoe ln CLIENT_READCAP COLLECTIVE_WRITECAP/NICKNAME
-    #
     # ...we're adding a sub-directory to the global DMD; the name of
     # this sub-directory is the invitee's name and it points to the
     # mutable directory we created for them (which will be included in
     # the invite code)
 
-    put_url = config.tahoe_client_url.child(u"uri")
-    put_url = put_url.child(magic_write_cap.decode("utf8"))
-    put_url = put_url.child(invitee_name)
-    put_url = put_url.add(u"t", u"uri")
-    put_url = put_url.add(u"replace", u"only-files")
-
-    resp = yield treq.put(put_url.to_text(), dmd_readonly_cap)
-    if resp.code < 200 or resp.code >= 300:
-        if resp.code == CONFLICT:
-            raise Exception(
-                "You cannot overwrite a directory with a file"
-                " (most likely participant '{}' already exists)".format(invitee_name)
-            )
-        else:
-            raise Exception(format_http_error("Error", resp))
+    yield tahoe_client.add_entry_to_mutable_directory(
+        magic_write_cap,
+        invitee_name,
+        dmd_readonly_cap,
+    )
 
     invite_code = "{}{}{}".format(
         magic_readonly_cap,
