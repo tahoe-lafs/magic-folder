9062586f2a4c8ea98487fea4abba9dc40680bf1e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-07-28 20:29:01 -0400

    Skip the deleted private helper

--- src/magic_folder/test/test_local_snapshot.py
+++ src/magic_folder/test/test_local_snapshot.py
@@ -260,7 +260,7 @@ class LocalSnapshotCreatorTests(SyncTestCase):
         for (file, content) in files:
             mangled_filename = path2magic(file.asTextMode(encoding="utf-8").path)
             stored_snapshot = self.db.get_local_snapshot(mangled_filename, self.author)
-            stored_content = stored_snapshot._get_synchronous_content()
+            stored_content = stored_snapshot.content_path.getContent()
             self.assertThat(stored_content, Equals(content))
             self.assertThat(stored_snapshot.parents_local, HasLength(0))
 
