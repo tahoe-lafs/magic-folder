6d9b8fc39e46797e914d2e3a6219f3de6020c2ae
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-08-05 22:46:37 +0530

    delete snapshot store as it is irrelevant

--- src/magic_folder/test/test_local_snapshot.py
+++ src/magic_folder/test/test_local_snapshot.py
@@ -20,8 +20,6 @@ from twisted.internet import defer
 from testtools.matchers import (
     Equals,
     Always,
-    HasLength,
-    MatchesStructure,
 )
 from testtools.twistedsupport import (
     succeeded,
@@ -36,16 +34,6 @@ from eliot import (
 
 from ..magic_folder import (
     LocalSnapshotService,
-    SnapshotStore,
-)
-from ..snapshot import (
-    create_local_author,
-)
-from ..magicpath import (
-    path2magic,
-)
-from ..config import (
-    create_global_configuration,
 )
 from .common import (
     SyncTestCase,
@@ -193,109 +181,4 @@ class LocalSnapshotServiceTests(SyncTestCase):
         self.assertThat(
             self.snapshot_service.stopService(),
             succeeded(Always())
-        )
-
-class SnapshotStoreTests(SyncTestCase):
-    """
-    Tests for ``SnapshotStore``, responsible for creating the local
-    snapshots and storing them in the database.
-    """
-    def setUp(self):
-        super(SnapshotStoreTests, self).setUp()
-        self.author = create_local_author("alice")
-
-    def setup_example(self):
-        """
-        Hypothesis-invoked hook to create per-example state.
-        Reset the database before running each test.
-        """
-        self.temp = FilePath(self.mktemp())
-        self.global_db = create_global_configuration(
-            self.temp.child(b"global-db"),
-            u"tcp:12345",
-            self.temp.child(b"tahoe-node"),
-        )
-        self.magic = self.temp.child(b"magic")
-        self.magic.makedirs()
-        self.db = self.global_db.create_magic_folder(
-            u"some-folder",
-            self.magic,
-            self.temp.child(b"state"),
-            self.author,
-            u"URI:DIR2-RO:aaa:bbb",
-            u"URI:DIR2:ccc:ddd",
-            60,
-        )
-        self.snapshot_creator = SnapshotStore(
-            db=self.db,
-            author=self.author,
-            stash_dir=self.db.stash_path,
-        )
-
-    @given(lists(path_segments().map(lambda p: p.encode("utf-8")), unique=True),
-           data())
-    def test_create_snapshots(self, filenames, data):
-        """
-        Create a list of filenames and random content as input and for each
-        of the (filename, content) mapping, create and store the snapshot in
-        the database.
-        """
-        files = []
-        for filename in filenames :
-            file = self.magic.child(filename)
-            content = data.draw(binary())
-            file.asBytesMode("utf-8").setContent(content)
-
-            files.append((file, content))
-
-        for (file, _unused) in files:
-            self.assertThat(
-                self.snapshot_creator.store_local_snapshot(file),
-                succeeded(Always())
-            )
-
-        self.assertThat(self.db.get_all_localsnapshot_paths(), HasLength(len(files)))
-        for (file, content) in files:
-            mangled_filename = path2magic(file.asTextMode(encoding="utf-8").path)
-            stored_snapshot = self.db.get_local_snapshot(mangled_filename, self.author)
-            stored_content = stored_snapshot.content_path.getContent()
-            self.assertThat(stored_content, Equals(content))
-            self.assertThat(stored_snapshot.parents_local, HasLength(0))
-
-    @given(content1=binary(min_size=1),
-           content2=binary(min_size=1),
-           filename=path_segments().map(lambda p: p.encode("utf-8")),
-    )
-    def test_create_snapshot_twice(self, filename, content1, content2):
-        """
-        If a snapshot already exists for a file, adding a new snapshot to it
-        should refer to the existing snapshot as a parent.
-        """
-        foo = self.magic.child(filename)
-        foo.asBytesMode("utf-8").setContent(content1)
-
-        # make sure the store_local_snapshot() succeeds
-        self.assertThat(
-            self.snapshot_creator.store_local_snapshot(foo),
-            succeeded(Always()),
-        )
-
-        foo_magicname = path2magic(foo.asTextMode('utf-8').path)
-        stored_snapshot1 = self.db.get_local_snapshot(foo_magicname, self.author)
-
-        # now modify the file with some new content.
-        foo.asBytesMode("utf-8").setContent(content2)
-
-        # make sure the second call succeeds as well
-        self.assertThat(
-            self.snapshot_creator.store_local_snapshot(foo),
-            succeeded(Always()),
-        )
-        stored_snapshot2 = self.db.get_local_snapshot(foo_magicname, self.author)
-
-        self.assertThat(
-            stored_snapshot2.parents_local[0],
-            MatchesStructure(
-                content_path=Equals(stored_snapshot1.content_path)
-            )
         )
