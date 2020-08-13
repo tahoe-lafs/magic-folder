aadf1d5bcb93f60dcb1c074f37b49134b4350f69
meejah <meejah@meejah.ca>
2020-08-12 19:28:44 -0600

    integration test for 'magic-folder migrate'

--- integration/test_general_cli.py
+++ integration/test_general_cli.py
@@ -56,3 +56,42 @@ def test_daemon_inititialize(request, reactor, temp_dir):
     assert config["magic_folders"] == dict()
     # the API token should at least be base64-decodable and result in 32 bytes of entropy
     assert len(base64.urlsafe_b64decode(config["api_token"].encode("utf8"))) == 32
+
+
+@pytest_twisted.inlineCallbacks
+def test_daemon_migrate(request, reactor, alice, temp_dir):
+    """
+    'magic-folder migrate' happy-path works
+    """
+
+    node_dir = join(temp_dir, "test-daemon-migrate")
+
+    print("\n\nabout to run magic-folder migrate\n")
+    proto = util._DumpOutputProtocol(None)
+    util._magic_folder_runner(
+        proto, reactor, request,
+        [
+            "--config", node_dir,
+            "migrate",
+            "--listen-endpoint", "tcp:1234",
+            "--node-directory", alice.node_directory,
+            "--author", "test",
+        ],
+    )
+    yield proto.done
+
+    proto = util._CollectOutputProtocol()
+    util._magic_folder_runner(
+        proto, reactor, request,
+        [
+            "show-config",
+            "--config", node_dir,
+        ],
+    )
+    output = yield proto.done
+    config = loads(output)
+
+    assert config["api_endpoint"] == "tcp:1234"
+    assert config["magic_folders"] == dict()
+    # the API token should at least be base64-decodable and result in 32 bytes of entropy
+    assert len(base64.urlsafe_b64decode(config["api_token"].encode("utf8"))) == 32
