0e34b309d848ac5bf8fb66756fa7f1a10fd9d73b
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-08-26 17:24:25 +0530

    a test for remote snapshot creation

--- src/magic_folder/test/test_magic_folder_service.py
+++ src/magic_folder/test/test_magic_folder_service.py
@@ -17,6 +17,7 @@ from twisted.python.filepath import (
 from twisted.application.service import (
     Service,
 )
+from twisted.internet import task
 from hypothesis import (
     given
 )
@@ -34,6 +35,7 @@ from testtools.twistedsupport import (
 from ..magic_folder import (
     MagicFolder,
     LocalSnapshotService,
+    UploaderService,
 )
 
 from .common import (
@@ -44,6 +46,9 @@ from .strategies import (
 )
 from .test_local_snapshot import (
     MemorySnapshotCreator as LocalMemorySnapshotCreator,
+)
+from .test_upload import (
+    MemorySnapshotCreator as RemoteMemorySnapshotCreator,
 )
 
 class MagicFolderServiceTests(SyncTestCase):
@@ -121,4 +126,74 @@ class MagicFolderServiceTests(SyncTestCase):
         self.assertThat(
             local_snapshot_creator.processed,
             Equals([target_path]),
+        )
+
+    @given(
+        relative_target_path=relative_paths(),
+        content=binary(),
+    )
+    def test_create_remote_snapshot(self, relative_target_path, content):
+        """
+        XXX
+        """
+        magic_path = FilePath(self.mktemp())
+        magic_path.makedirs()
+        target_path = magic_path.preauthChild(relative_target_path).asBytesMode("utf-8")
+        target_path.parent().makedirs(ignoreExistingDirectory=True)
+        target_path.setContent(content)
+
+        local_snapshot_creator = LocalMemorySnapshotCreator()
+        local_snapshot_service = LocalSnapshotService(magic_path, local_snapshot_creator)
+        clock = task.Clock()
+        poll_interval = 1  # XXX: this would come from config.
+
+        # create RemoteSnapshotCreator and UploaderService
+        remote_snapshot_creator = RemoteMemorySnapshotCreator()
+        uploader_service = UploaderService(
+            poll_interval=poll_interval,
+            clock=clock,
+            remote_snapshot_creator=remote_snapshot_creator,
+        )
+
+        tahoe_client = object()
+        name = u"local-snapshot-service-test"
+        config = object()
+        participants = object()
+        magic_folder = MagicFolder(
+            client=tahoe_client,
+            config=config,
+            name=name,
+            local_snapshot_service=local_snapshot_service,
+            uploader_service=uploader_service,
+            initial_participants=participants,
+            clock=clock,
+        )
+        magic_folder.startService()
+        self.addCleanup(magic_folder.stopService)
+
+        # One upload attempt at the service start
+        self.assertThat(
+            remote_snapshot_creator._uploaded,
+            Equals(1),
+        )
+
+        adding = magic_folder.local_snapshot_service.add_file(
+            target_path,
+        )
+        self.assertThat(
+            adding,
+            succeeded(Always()),
+        )
+
+        self.assertThat(
+            local_snapshot_creator.processed,
+            Equals([target_path]),
+        )
+
+        # advance the clock and assert that the remote snapshot got
+        # created.
+        clock.advance(poll_interval)
+        self.assertThat(
+            remote_snapshot_creator._uploaded,
+            Equals(2),
         )
