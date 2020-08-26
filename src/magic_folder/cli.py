fc96effc770ed6a0402e5f09aadc4b39537dc607
meejah <meejah@meejah.ca>
2020-08-26 16:25:47 -0600

    sys.argv isn't unicode

--- src/magic_folder/cli.py
+++ src/magic_folder/cli.py
@@ -206,7 +206,7 @@ def initialize(options):
 
     yield magic_folder_initialize(
         options.parent._config_path,
-        options['listen-endpoint'],
+        options['listen-endpoint'].decode("utf8"),
         FilePath(options['node-directory']),
         options['client-endpoint'],
     )
@@ -264,7 +264,7 @@ def migrate(options):
 
     config = yield magic_folder_migrate(
         options.parent._config_path,
-        options['listen-endpoint'],
+        options['listen-endpoint'].decode("utf8"),
         FilePath(options['node-directory']),
         options['author'],
         options['client-endpoint'],
