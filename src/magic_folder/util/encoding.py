029da59fbb321e66f0c98425f313050161a9a68b
meejah <meejah@meejah.ca>
2021-05-24 15:39:51 -0600

    docstring, copyright

--- src/magic_folder/util/encoding.py
+++ src/magic_folder/util/encoding.py
@@ -1,4 +1,18 @@
+# Copyright 2021 The Magic-Folder Developers
+# See COPYING for details.
+
+from __future__ import (
+    print_function,
+    unicode_literals,
+)
+
 import unicodedata
 
+
 def normalize(text):
+    """
+    :param unicode text: some unicode string
+
+    :returns: the normalized form of the unicode in `text`.
+    """
     return unicodedata.normalize("NFC", text)
