b3a7b7e7dde1eefd68c6cf2ba561bbe12366c21d
meejah <meejah@meejah.ca>
2021-05-25 02:54:16 -0600

    add some websocket tests

--- src/magic_folder/test/test_status.py
+++ src/magic_folder/test/test_status.py
@@ -9,10 +9,24 @@ from testtools.matchers import (
     Equals,
 )
 
+from autobahn.twisted.testing import (
+    create_memory_agent,
+    MemoryReactorClockResolver,
+    create_pumper,
+)
+from autobahn.twisted.websocket import (
+    WebSocketClientProtocol,
+)
+from twisted.internet.defer import (
+    inlineCallbacks,
+)
+
 from .common import (
     SyncTestCase,
+    AsyncTestCase,
 )
 from ..status import (
+    StatusFactory,
     WebSocketStatusService,
 )
 
@@ -117,4 +131,96 @@ class StatusServiceTests(SyncTestCase):
                     "synchronizing": True,
                 }
             }])
+        )
+
+
+class WebSocketTests(AsyncTestCase):
+    """
+    Tests relating to the actual WebSocket protocol of the status
+    serice
+    """
+
+    def setUp(self):
+        super(WebSocketTests, self).setUp()
+        self.reactor = MemoryReactorClockResolver()
+        self.pumper = create_pumper()
+        self.service = WebSocketStatusService()
+        self.factory = StatusFactory(self.service)
+        self.agent = create_memory_agent(
+            self.reactor,
+            self.pumper,
+            lambda: self.factory.buildProtocol(None)
+        )
+        return self.pumper.start()
+
+    def tearDown(self):
+        super(WebSocketTests, self).tearDown()
+        return self.pumper.stop()
+
+    def test_open(self):
+        """
+        """
+
+        messages = []
+
+        class TestProto(WebSocketClientProtocol):
+            def onMessage(self, msg, is_binary):
+                messages.append(json.loads(msg))
+
+        # upon open, we should receive the current state
+        self.agent.open("ws://127.0.0.1:-1/v1/status", {}, TestProto)
+        self.pumper._flush()
+        self.assertThat(
+            messages,
+            Equals([
+                {
+                    "state": {
+                        "synchronizing": False,
+                    }
+                }
+            ])
+        )
+
+        # if we change the state, we should receive an update
+        self.service.upload_started()
+        self.pumper._flush()
+        self.assertThat(
+            messages,
+            Equals([
+                {
+                    "state": {
+                        "synchronizing": False,
+                    }
+                },
+                {
+                    "state": {
+                        "synchronizing": True,
+                    }
+                }
+            ])
+        )
+
+    def test_send_message(self):
+        """
+        Sending a message is a protocol error
+        """
+
+        closed = []
+
+        class TestProto(WebSocketClientProtocol):
+            def onOpen(self):
+                self.sendMessage(b"bogus")
+
+            def onClose(self, was_clean, code, reason):
+                closed.append(reason)
+
+        # we send a message, which is a protocol violation .. so we
+        # should see a disconnect
+        self.agent.open("ws://127.0.0.1:-1/v1/status", {}, TestProto)
+        self.pumper._flush()
+        self.assertThat(
+            closed,
+            Equals([
+                "Unexpected incoming message",
+            ])
         )
