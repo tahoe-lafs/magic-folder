8561dfb75022dc4ee78b48029dd2b3255aa21c17
Tom Prince <tom.prince@leastauthority.com>
2021-05-31 13:56:42 -0600

    Use archive.org mirror.

--- .github/vcpython27/tools/chocolateyinstall.ps1
+++ .github/vcpython27/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'vcpython27'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+$url        = 'https://web.archive.org/web/20200810223604if_/https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
 
 $packageArgs = @{
   packageName   = $packageName
