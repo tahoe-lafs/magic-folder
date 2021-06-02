be343e8894ed96a2ac36aade419f37281afd3c1a
Tom Prince <tom.prince@leastauthority.com>
2021-06-02 14:35:46 -0600

    Limit folder-names used in tests.
    We should also handle or disallow various characters in code, but that will be
    handled in https://github.com/LeastAuthority/magic-folder/issues/369

--- src/magic_folder/test/strategies.py
+++ src/magic_folder/test/strategies.py
@@ -99,6 +99,8 @@ FOLDER_ALPHABET = characters(
     blacklist_categories=(
         # Exclude surrogates.  They're complicated.
         "Cs",
+        # FIXME: https://github.com/LeastAuthority/magic-folder/issues/369
+        "Cc",
     ),
     blacklist_characters=(u"\x00", u"/"),
 )
@@ -213,6 +215,9 @@ def folder_names():
         min_size=1,
     ).map(
         normalize,
+    ).filter(
+        # FIXME: https://github.com/LeastAuthority/magic-folder/issues/369
+        _valid_path_segment
     )
 
 
