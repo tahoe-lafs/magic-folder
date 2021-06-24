c37546b7ddb2ed1f57c26d8eca6fdda597c23863
meejah <meejah@meejah.ca>
2021-06-24 16:50:47 -0600

    we don't download ignored files

--- src/magic_folder/test/test_download.py
+++ src/magic_folder/test/test_download.py
@@ -908,6 +908,5 @@ class ConflictTests(AsyncTestCase):
         self.assertThat(
             self.filesystem.actions,
             Equals([
-                ("download", parent),
             ])
         )
