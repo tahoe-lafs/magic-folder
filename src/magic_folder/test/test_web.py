9865c77cb547dcf898ec455b620adeeef7d56aae
meejah <meejah@meejah.ca>
2021-06-25 01:23:38 -0600

    restore

--- src/magic_folder/test/test_web.py
+++ src/magic_folder/test/test_web.py
@@ -834,8 +834,19 @@ class CreateSnapshotTests(SyncTestCase):
             # our request to receive a response.
             start_folder_services=True,
         )
-        # because we are starting services, the scanner will run and
-        # discover 'some_file', creating a snapshot
+        self.assertThat(
+            authorized_request(
+                treq,
+                AUTH_TOKEN,
+                b"POST",
+                self.url.child(folder_name, "snapshot").set(u"path", path_in_folder),
+            ),
+            succeeded(
+                matches_response(
+                    code_matcher=Equals(CREATED),
+                ),
+            ),
+        )
 
         self.assertThat(
             authorized_request(
