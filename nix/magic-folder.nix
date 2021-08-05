6e88e3b8cb3dca199f7c00cdbd40e3d7e9d83c3a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-08-05 10:47:07 -0400

    Get rid of all of the Git-derived auto-versioning code
    magic-folder is now version 0.1.0

--- nix/magic-folder.nix
+++ nix/magic-folder.nix
@@ -1,7 +1,7 @@
 { lib, python, buildPythonPackage, pythonPackages, tahoe-lafs, klein, git }:
 buildPythonPackage rec {
   pname = "magic-folder";
-  version = "2021.7.30.dev0";
+  version = "0.1.0";
   src = lib.cleanSource ../.;
 
   propagatedBuildInputs = with pythonPackages; [
@@ -15,23 +15,6 @@ buildPythonPackage rec {
     testtools
     fixtures
   ];
-
-  postPatch = ''
-    # Generate _version.py ourselves since we can't rely on the Python code
-    # extracting the information from the .git directory that is likely not
-    # available.
-    cat > src/magic_folder/_version.py <<EOF
-
-# This _version.py is generated from metadata by nix/magic-folder.nix.
-
-__pkgname__ = "magic-folder"
-real_version = "${version}"
-full_version = "${version}"
-branch = "main"
-verstr = "${version}"
-__version__ = verstr
-EOF
-  '';
 
   checkPhase = ''
     export MAGIC_FOLDER_HYPOTHESIS_PROFILE="magic-folder-ci"
--- setup.py
+++ setup.py
@@ -17,25 +17,9 @@ import sys
 #
 # See the docs/about.rst file for licensing information.
 
-import os, subprocess, re
+import os
 
 basedir = os.path.dirname(os.path.abspath(__file__))
-
-# locate our version number
-
-def read_version_py(infname):
-    try:
-        verstrline = open(infname, "rt").read()
-    except EnvironmentError:
-        return None
-    else:
-        VSRE = r"^verstr = ['\"]([^'\"]*)['\"]"
-        mo = re.search(VSRE, verstrline, re.M)
-        if mo:
-            return mo.group(1)
-
-VERSION_PY_FILENAME = 'src/magic_folder/_version.py'
-version = read_version_py(VERSION_PY_FILENAME)
 
 def load_requirements(filename):
     with open(os.path.join(basedir, "requirements", filename), "r") as f:
@@ -88,144 +72,6 @@ trove_classifiers = [
     ]
 
 
-GIT_VERSION_BODY = '''
-# This _version.py is generated from git metadata by the magic_folder setup.py.
-
-__pkgname__ = "%(pkgname)s"
-real_version = "%(version)s"
-full_version = "%(full)s"
-branch = "%(branch)s"
-verstr = "%(normalized)s"
-__version__ = verstr
-'''
-
-def run_command(args, cwd=None):
-    use_shell = sys.platform == "win32"
-    try:
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=cwd, shell=use_shell)
-    except EnvironmentError as e:
-        print("Warning: unable to run %r." % (" ".join(args),))
-        print(e)
-        return None
-    stdout = p.communicate()[0].strip()
-    if p.returncode != 0:
-        print("Warning: %r returned error code %r." % (" ".join(args), p.returncode))
-        return None
-    return stdout
-
-
-def versions_from_git(tag_prefix):
-    # This runs 'git' from the directory that contains this file. That either
-    # means someone ran a setup.py command (and this code is in
-    # versioneer.py, thus the containing directory is the root of the source
-    # tree), or someone ran a project-specific entry point (and this code is
-    # in _version.py, thus the containing directory is somewhere deeper in
-    # the source tree). This only gets called if the git-archive 'subst'
-    # variables were *not* expanded, and _version.py hasn't already been
-    # rewritten with a short version string, meaning we're inside a checked
-    # out source tree.
-
-    # versions_from_git (as copied from python-versioneer) returns strings
-    # like "1.9.0-25-gb73aba9-dirty", which means we're in a tree with
-    # uncommited changes (-dirty), the latest checkin is revision b73aba9,
-    # the most recent tag was 1.9.0, and b73aba9 has 25 commits that weren't
-    # in 1.9.0 . The narrow-minded NormalizedVersion parser that takes our
-    # output (meant to enable sorting of version strings) refuses most of
-    # that. magic_folder uses a function named suggest_normalized_version()
-    # that can handle "1.9.0.post25", so dumb down our output to match.
-
-    try:
-        source_dir = os.path.dirname(os.path.abspath(__file__))
-    except NameError as e:
-        # some py2exe/bbfreeze/non-CPython implementations don't do __file__
-        print("Warning: unable to find version because we could not obtain the source directory.")
-        print(e)
-        return {}
-    stdout = run_command(["git", "describe", "--tags", "--dirty", "--always"],
-                         cwd=source_dir)
-    if stdout is None:
-        # run_command already complained.
-        return {}
-    stdout = stdout.decode("ascii")
-    if not stdout.startswith(tag_prefix):
-        print("Warning: tag %r doesn't start with prefix %r." % (stdout, tag_prefix))
-        return {}
-    version = stdout[len(tag_prefix):]
-    pieces = version.split("-")
-    if len(pieces) == 1:
-        normalized_version = pieces[0]
-    else:
-        normalized_version = "%s.post%s" % (pieces[0], pieces[1])
-
-    stdout = run_command(["git", "rev-parse", "HEAD"], cwd=source_dir)
-    if stdout is None:
-        # run_command already complained.
-        return {}
-    full = stdout.decode("ascii").strip()
-    if version.endswith("-dirty"):
-        full += "-dirty"
-        normalized_version += ".dev0"
-
-    # Thanks to Jistanidiot at <http://stackoverflow.com/questions/6245570/get-current-branch-name>.
-    stdout = run_command(["git", "rev-parse", "--abbrev-ref", "HEAD"], cwd=source_dir)
-    branch = (stdout or b"unknown").decode("ascii").strip()
-
-    # this returns native strings (bytes on py2, unicode on py3)
-    return {"version": version, "normalized": normalized_version,
-            "full": full, "branch": branch}
-
-# setup.cfg has an [aliases] section which runs "update_version" before many
-# commands (like "build" and "sdist") that need to know our package version
-# ahead of time. If you add different commands (or if we forgot some), you
-# may need to add it to setup.cfg and configure it to run update_version
-# before your command.
-
-class UpdateVersion(Command):
-    description = "update _version.py from revision-control metadata"
-    user_options = install.install.user_options
-
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        global version
-        verstr = version
-        if os.path.isdir(os.path.join(basedir, ".git")):
-            verstr = self.try_from_git()
-
-        if verstr:
-            self.distribution.metadata.version = verstr
-        else:
-            print("""\
-********************************************************************
-Warning: no version information found. This may cause tests to fail.
-********************************************************************
-""")
-
-    def try_from_git(self):
-        # If we change the release tag names, we must change this too
-        versions = versions_from_git("magic-folder-")
-
-        # setup.py might be run by either py2 or py3 (when run by tox, which
-        # uses py3 on modern debian/ubuntu distros). We want this generated
-        # file to contain native strings on both (str=bytes in py2,
-        # str=unicode in py3)
-        if versions:
-            body = GIT_VERSION_BODY % {
-                "pkgname": self.distribution.get_name(),
-                "version": versions["version"],
-                "normalized": versions["normalized"],
-                "full": versions["full"],
-                "branch": versions["branch"],
-                }
-            f = open(VERSION_PY_FILENAME, "wb")
-            f.write(body.encode("ascii"))
-            f.close()
-            print("Wrote normalized version %r into '%s'" % (versions["normalized"], VERSION_PY_FILENAME))
-
-        return versions.get("normalized", None)
-
 class PleaseUseTox(Command):
     user_options = []
     def initialize_options(self):
@@ -237,19 +83,15 @@ class PleaseUseTox(Command):
         print("ERROR: Please use 'tox' to run the test suite.")
         sys.exit(1)
 
-setup_args = {}
-if version:
-    setup_args["version"] = version
-
 setup(name="magic_folder",
+      version="0.1.0",
       description='Tahoe-LAFS-based file synchronization',
       long_description=open('README.rst', 'rU').read(),
       author='the Tahoe-LAFS developers, the Magic-Folder developers',
       author_email='tahoe-dev@tahoe-lafs.org',
       url='https://github.com/LeastAuthority/magic_folder/',
       license='GNU GPL', # see README.rst -- there is an alternative licence
-      cmdclass={"update_version": UpdateVersion,
-                "test": PleaseUseTox,
+      cmdclass={"test": PleaseUseTox,
                 },
       package_dir={'': 'src'},
       packages=find_packages('src') + ["twisted.plugins", "magic_folder.test.plugins"],
@@ -269,5 +111,4 @@ setup(name="magic_folder",
               "magic-folder-api = magic_folder.api_cli:_entry",
           ],
       },
-      **setup_args
 )
