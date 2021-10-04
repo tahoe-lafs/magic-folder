195aa28a50c7939640e4852566c783555df2b49f
meejah <meejah@meejah.ca>
2021-10-03 21:33:37 -0600

    test for local-snapshot-store deletion

--- src/magic_folder/test/test_local_snapshot.py
+++ src/magic_folder/test/test_local_snapshot.py
@@ -389,3 +389,34 @@ class LocalSnapshotCreatorTests(SyncTestCase):
                 content_path=Equals(stored_snapshot1.content_path)
             )
         )
+
+    @given(content=binary(min_size=1),
+           filename=path_segments(),
+    )
+    def test_delete_snapshot(self, filename, content):
+        """
+        Create a snapshot and then a deletion snapshot of it.
+        """
+        foo = self.magic.child(filename)
+        foo.asBytesMode("utf-8").setContent(content)
+
+        # make sure the store_local_snapshot() succeeds
+        self.assertThat(
+            self.snapshot_creator.store_local_snapshot(foo),
+            succeeded(Always()),
+        )
+
+        # delete the file
+        foo.asBytesMode("utf-8").remove()
+
+        # store a new snapshot
+        self.assertThat(
+            self.snapshot_creator.store_local_snapshot(foo),
+            succeeded(Always()),
+        )
+        stored_snapshot2 = self.db.get_local_snapshot(filename)
+
+        self.assertThat(
+            stored_snapshot2.is_delete(),
+            Equals(True),
+        )
