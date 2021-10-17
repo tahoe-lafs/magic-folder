2ecedc5f4aa795c310530c0ba6353a728f38685c
meejah <meejah@meejah.ca>
2021-10-17 00:39:40 -0600

    more utest

---
+++ src/magic_folder/test/test_client.py
@@ -0,0 +1,83 @@
+# Copyright 2020 Least Authority TFA GmbH
+# See COPYING for details.
+
+"""
+Tests for ``magic_folder.tahoe_client``.
+"""
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+from testtools.matchers import (
+    Equals,
+    Always,
+)
+
+from testtools.twistedsupport import (
+    succeeded,
+)
+from treq.testing import (
+    StubTreq,
+    StringStubbingResource,
+)
+
+from ..client import (
+    MagicFolderClient,
+)
+from .common import (
+    SyncTestCase,
+)
+
+
+class MagicFolderClientTests(SyncTestCase):
+    """
+    Tests for MagicFolderClient
+    """
+    def setup_client(self):
+        """
+        Create a fake Tahoe-LAFS web frontend and a client that will talk to it.
+        """
+
+        self.api_calls = []
+
+        def get_resource_for(method, url, params, headers, data):
+            self.api_calls.append((method, url, params, headers, data))
+            return (200, {}, b"{}")
+
+        self.client = MagicFolderClient(
+            StubTreq(StringStubbingResource(get_resource_for)),
+            lambda: "fake token",
+        )
+
+    def setUp(self):
+        super(MagicFolderClientTests, self).setUp()
+        self.setup_client()
+
+    def test_tahoe_objects(self):
+        """
+        The /tahoe-objects API works
+        """
+        self.assertThat(
+            self.client.tahoe_objects("a_magic_folder"),
+            succeeded(Always()),
+        )
+        self.assertThat(
+            self.api_calls,
+            Equals([
+                ('GET',
+                 'http://invalid./v1/magic-folder/a_magic_folder/tahoe-objects',
+                 {},
+                 {
+                     'Accept-Encoding': ['gzip'],
+                     'Authorization': ['Bearer fake token'],
+                     'Connection': ['close'],
+                     'Host': ['invalid.'],
+                 },
+                 '',
+                ),
+            ])
+        )
