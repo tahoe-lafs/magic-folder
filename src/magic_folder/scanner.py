cb0a64e176d3e4b3af5c8c799293e280ef159c65
meejah <meejah@meejah.ca>
2021-11-02 12:33:32 -0600

    flake8

--- src/magic_folder/scanner.py
+++ src/magic_folder/scanner.py
@@ -14,7 +14,10 @@ from eliot import (
 )
 from eliot.twisted import inline_callbacks
 from twisted.application.service import MultiService
-from twisted.internet.defer import DeferredLock
+from twisted.internet.defer import (
+    DeferredLock,
+    gatherResults,
+)
 from twisted.internet.task import Cooperator
 
 from .util.file import get_pathinfo
