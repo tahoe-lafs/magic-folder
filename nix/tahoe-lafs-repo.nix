2c9e7deb673b0b8936d8f39332ff9c427a7f84a8
meejah <meejah@meejah.ca>
2021-12-06 13:50:42 -0700

    remove nix from CI

--- nix/tahoe-lafs-repo.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.fetchFromGitHub {
-    owner = "tahoe-lafs";
-    repo = "tahoe-lafs";
-    rev = "tahoe-lafs-1.15.1";
-    sha256 = "1kaz21gljxwwldfs8bigyzvqs1h70d66jlj01b6m2bwn98l50m0s";
-  }
