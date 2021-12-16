e817276d1e65dfb9c2b81c89dbca7d0d1f7083e3
meejah <meejah@meejah.ca>
2021-12-15 17:49:34 -0700

    dead code

--- src/magic_folder/magicpath.py
+++ src/magic_folder/magicpath.py
@@ -27,26 +27,3 @@ def magic2path(path):
         return re.sub(u'@.', lambda m: {u'@_': u'/', u'@@': u'@'}[m.group(0)], path)
     except KeyError as e:
         raise InvalidMangledPath(path, str(e))
-
-
-
-IGNORE_SUFFIXES = [u'.backup', u'.tmp', u'.conflict']
-IGNORE_PREFIXES = [u'.']
-
-def should_ignore_file(path_u):
-    precondition(isinstance(path_u, unicode), path_u=path_u)
-
-    for suffix in IGNORE_SUFFIXES:
-        if path_u.endswith(suffix):
-            return True
-
-    while path_u != u"":
-        oldpath_u = path_u
-        path_u, tail_u = os.path.split(path_u)
-        if tail_u.startswith(u"."):
-            return True
-        if path_u == oldpath_u:
-            return True  # the path was absolute
-        _assert(len(path_u) < len(oldpath_u), path_u=path_u, oldpath_u=oldpath_u)
-
-    return False
--- src/magic_folder/test/test_magicpath.py
+++ src/magic_folder/test/test_magicpath.py
@@ -47,7 +47,6 @@ from ..magicpath import (
     InvalidMangledPath,
     path2magic,
     magic2path,
-    should_ignore_file,
 )
 
 
@@ -76,57 +75,4 @@ class MagicPath(SyncTestCase):
         self.assertThat(
             str(InvalidMangledPath("@invalid", "sequence error")),
             StartsWith("Invalid escape sequence")
-        )
-
-    @given(relative_paths(), sampled_from([u"backup", u"tmp", u"conflict"]))
-    def test_ignore_known_suffixes(self, path, suffix):
-        """
-        Relative paths ending with certain well-known suffixes are ignored.
-        """
-        path += u"." + suffix
-        self.assertThat(
-            path,
-            AfterPreprocessing(should_ignore_file, Equals(True)),
-        )
-
-    @given(randoms(), lists(path_segments(), min_size=1))
-    def test_ignore_dotfiles(self, random, path_segments):
-        """
-        Relative paths involving dotfiles are ignored.
-        """
-        index = random.randrange(len(path_segments))
-        path_segments[index] = u"." + path_segments[index]
-        path = join(*path_segments)
-        self.assertThat(
-            path,
-            AfterPreprocessing(should_ignore_file, Equals(True)),
-        )
-
-    @given(absolute_paths())
-    def test_ignore_absolute_paths(self, path):
-        """
-        Absolute paths are ignored.
-        """
-        self.assertThat(
-            path,
-            AfterPreprocessing(should_ignore_file, Equals(True)),
-        )
-
-    @given(relative_paths(path_segments_without_dotfiles()))
-    def test_dont_ignore_others(self, path):
-        """
-        Relative paths not involving dotfiles and without certain well-known
-        suffixes are not ignored.
-        """
-        assume(u"/." not in path)
-        assume(
-            not any(
-                path.endswith(suffix)
-                for suffix
-                in [u".backup", u".tmp", u".conflict"]
-            )
-        )
-        self.assertThat(
-            path,
-            AfterPreprocessing(should_ignore_file, Equals(False)),
         )
